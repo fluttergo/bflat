6cc2e6fccd7f8d458ddf7dca6f91fcb4a5d7bd68
waverly <weifeng@yudesports.com>
2024-01-17 15:45:55 +0800

    build sample for android

--- samples/MinimalSize/README.md
+++ samples/MinimalSize/README.md
@@ -7,3 +7,57 @@ $ bflat build minimalsize.cs --no-reflection --no-stacktrace-data --no-globaliza
 ```
 
 This will produce a minimalsize(.exe) file that is native compiled. You can launch it. Observe the difference in runtime behavior and size of the output when you omit some of the arguments from the `bflat build` command line above.
+
+
+```shell
+#编译成android可执行文件
+bflat build minimalsize.cs --no-reflection --no-stacktrace-data --no-globalization --no-exception-messages -Os  --separate-symbols --libc bionic  --arch arm64  --os linux
+````
+
+```shell
+#安装此程序到android设备
+adb push D:\workSpace\bflat\samples\MinimalSize\minimalsize /data/local/tmp/
+```
+
+
+
+```shell
+#运行此程序
+chmod +x minimalsize
+
+./minimalsize
+
+
+
+```
+
+输出如下内容:
+
+```
+
+NullReferenceException message is: Arg_NullReferenceException
+The runtime type of int is named: MT399534553776
+Type of boxed integer is equal to typeof(int)
+Type of boxed integer is not equal to typeof(byte)
+Upper case of 'Вторник' is 'ВТОРНИК'
+Current stack frame is minimalsize!<BaseAddress>+0x17c1c4 at offset 644 in file:line:column <filename unknown>:0:0
+
+await start
+[Thread-2] :0
+[Thread-2] :1
+[Thread-2] :2
+await delay 3 s
+[Thread-2] :3
+[Thread-2] :4
+[Thread-2] :5
+await delay 2 s
+[Thread-2] :6
+[Thread-2] :7
+[Thread-2] :8
+await delay 1 s
+exit:9
+[Thread-2] :9
+Standard Output:
+total 18590
+-rwxrwxrwx 1 shell shell  1797504 2024-01-17 15:36 minimalsize
+```
--- samples/MinimalSize/minimalsize.cs
+++ samples/MinimalSize/minimalsize.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 Console.WriteLine($"NullReferenceException message is: {new NullReferenceException().Message}");
 Console.WriteLine($"The runtime type of int is named: {typeof(int)}");
@@ -7,3 +10,72 @@ Console.WriteLine($"Type of boxed integer is{(123.GetType() == typeof(int) ? "" 
 Console.WriteLine($"Type of boxed integer is{(123.GetType() == typeof(byte) ? "" : " not")} equal to typeof(byte)");
 Console.WriteLine($"Upper case of 'Вторник' is '{"Вторник".ToUpper()}'");
 Console.WriteLine($"Current stack frame is {new StackTrace().GetFrame(0)}");
+
+Console.WriteLine($"await start ");
+
+
+
+bool isStop = false;
+List<String> list = new List<string>();
+new Thread(() => {
+    int i = 0;
+
+    while (!isStop) {
+        Thread.Sleep(300);
+        list.Add($"${i}");
+        Console.WriteLine($"[{Thread.CurrentThread.Name}] :{i}");
+        i++;
+    }
+}) { Name = "Thread-2" }.Start();
+
+
+
+for (int i = 3; i > 0; i--) {
+    await Task.Delay(1000);
+    Console.WriteLine($"await delay {i} s");
+}
+
+
+
+isStop = true;
+Console.WriteLine($"exit:{list.Count}");
+
+
+// 要执行的 shell 命令
+string command = "su -c ls -l";
+
+// 创建一个 ProcessStartInfo 对象，用于设置进程启动的相关信息
+ProcessStartInfo psi = new ProcessStartInfo("sh", $"-c \"{command}\"")
+{
+    RedirectStandardOutput = true,
+    RedirectStandardError = true,
+    UseShellExecute = false,
+    CreateNoWindow = true
+};
+
+// 创建一个 Process 对象
+Process process = new Process { StartInfo = psi };
+
+try
+{
+    // 启动进程
+    process.Start();
+
+    // 读取标准输出和标准错误流
+    string output = process.StandardOutput.ReadToEnd();
+    string error = process.StandardError.ReadToEnd();
+
+    // 等待进程退出
+    process.WaitForExit();
+
+    // 输出结果
+    Console.WriteLine("Standard Output:");
+    Console.WriteLine(output);
+
+    Console.WriteLine("Standard Error:");
+    Console.WriteLine(error);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
